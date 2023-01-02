750786e293d692aebfc9e4c945f265d7495485e4
HopeFullBlack <soniq.sq@outlook.fr>
2023-01-02 14:57:34 +0100

    commit pre ckeditor

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,21 +4,38 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Test\TypeTestCase;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('full_name')
-            ->add('email')
+            ->add('full_name', TextType::class, [
+                'label'=> false,
+                'attr'=>[
+                     "placeholder"=> "Votre nom",
+                     "class"=> "flex-1"  
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'label'=> false,
+                "attr" => [
+                    "placeholder" => "Votre email ...",
+                    "class" => "flex-1"
+                ]
+            ])
+
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
@@ -27,11 +44,15 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
-            ->add('plainPassword', PasswordType::class, [
+            ->add('plainPassword', RepeatedType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
+                'label'=> false,
+                'type'=> PasswordType::class,
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'attr' => [
+                    "placeholder"=> "Mot de passe",
+                    'autocomplete' => 'new-password'],
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter a password',
@@ -43,7 +64,18 @@ class RegistrationFormType extends AbstractType
                         'max' => 4096,
                     ]),
                 ],
-            ])
+                'first_options'  => ['label' => false,
+                'attr'=>[
+                'placeholder'=>'Entrez un mot de passe'
+            ]
+        ],
+
+                'second_options' => ['label' => false,
+                'attr'=>[
+                    'placeholder'=>'Confirmez votre mot de passe'
+                ]
+            ],
+            ])  
         ;
     }
 
